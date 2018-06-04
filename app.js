5c9b38ad385537d861811bd04c3527ca5fbc6f85
Devesh Anand <ananddevesh29.devp@outlook.com>
2018-06-05 00:50:02 +0530

    cors middleware added

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 require('dotenv').config();
 var express = require('express');
 var bodyParser = require('body-parser');
+var cors = require('cors');
 var db = require('./src/app/lib/sadda-db');
 var version_router = require('./version_router');
 
 var app = express();
  
 var port = process.env.PORT ||  3000;
+
+app.use(cors());
 
 app.use( bodyParser.json() );       // to support JSON-encoded bodies
 app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
