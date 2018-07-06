b94c6396ee7ce28d19ed1acebab78726659673d5
Devesh Anand <ananddevesh29.devp@outlook.com>
2018-07-06 19:17:36 +0530

    useranalytics spec improvements

--- src/admin/analytics/test/UserAnalytics.spec.js
+++ src/admin/analytics/test/UserAnalytics.spec.js
@@ -3,52 +3,55 @@ var UserAnalytics = require('./../membership/UserAnalytics');
 var db = require('./../../../app/lib/sadda-db');
 var User = require("./../../../app/models/User")
 
-describe.only("UserAnalytics" , function(){
+describe("UserAnalytics" , function(){
+	var testSetup = function(callback){
+		var args = {
+			country_code : "+91",
+			number : "9162728446",
+			username : "akash",
+			profilepic : " "
+		}
+		User.create(args , function(error, result){
+			callback(error, result);
+		});
+	}
+	before(function(done){
+		db.connect(db.MODE_TEST, function(){
+			db.drop([db.tables.users.name], function(){
+				done();
+			});
+		});
+	});
     describe("Get Total User Count" , function(){
+		var res;
         before(function(done){
-            db.connect(db.MODE_TEST, function(){
-                db.drop([db.tables.users.name], function(){
-                });
-            }); 
-            var args = {
-                country_code : "+91",
-                number : "9162728446",
-                username : "akash",
-                profilepic : " "
-            }
-            User.create(args , function(){
-                done();
-            });
-        });
-        describe("get user count" , function(){
-            var res = { userCount : undefined };
-            var success ; 
-            var err;
-            before(function(done){
-                    UserAnalytics.getUserCount(function(error , result){
-                    err=error;
-                    res.userCount = result.totaluserCount;
-                    success = result.success;
-                    done();
-                });
-            });
+			testSetup(function(error){
+				if(error)throw(error);
+				UserAnalytics.getUserCount(function(result){
+					res=result;
+					done();
+				});
+			});
+		});
+		it("correct user count is returned" , function(){
+			assert.ok(res.totalUserCount === 1);
+		});
+		it("success is true" , function(){
+			assert.ok(res.success);
+		});
 
-            it("check for error" , function(){
-                assert.ok(!err);
-            });
-            it("check for result" , function(){
-                assert.ok(res.userCount == 1);
-            });
-            it("check success" , function(){
-                assert.ok(success);
-            });
-        });
-        after(function(done){
-            db.drop([db.tables.users.name], function(){
-                done();
-            });
-        });
-    }); 
+	});
+	afterEach(function(done){
+		db.drop([db.tables.users.name], function(){
+			done();
+		});
+	});
+	after(function(done){
+		db.drop([db.tables.users.name], function(){
+			db.end();
+			done();
+		});
+	});
 });
 
 
