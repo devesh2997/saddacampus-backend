5ea3753d907533bc0ab9bd368f700b468dc7c0f9
Devesh Anand <ananddevesh29.devp@outlook.com>
2018-07-06 16:30:59 +0530

    minor

--- src/admin/router.js
+++ src/admin/router.js
@@ -8,7 +8,7 @@ var MerchantManagement = require('./merchant-management');
 var Analytics = require('./analytics');
 
 router.use('/membership',adminMembership.router);
-router.use('/merchant',MerchantManagement.router);
+router.use('/merchants',MerchantManagement.router);
 router.use('/analytics',Analytics.router);
 
 module.exports = router;
