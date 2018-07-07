b29bf47688b9e59a151c72da9d43a6dbe556a347
akash <akashagarwal0403@gmail.com>
2018-07-07 18:49:47 +0530

    modified test

--- src/admin/analytics/test/UserAnalytics.spec.js
+++ src/admin/analytics/test/UserAnalytics.spec.js
@@ -1,7 +1,8 @@
 var assert = require('assert');
 var UserAnalytics = require('./../membership/UserAnalytics');
 var db = require('./../../../app/lib/sadda-db');
-var User = require("./../../../app/models/User")
+var User = require("./../../../app/models/User");
+var moment = require("moment");
 
 describe.only("UserAnalytics" , function(){
 	var testSetup = function(callback){
@@ -42,42 +43,110 @@ describe.only("UserAnalytics" , function(){
 
     });
     describe("Get Total User Registered previous week" , function(){
-		var res;
-        before(function(done){
-            testSetup(function(error){
-				if(error)throw(error);
-				UserAnalytics.previousWeekRegistered(function(error , result){
-					res=result;
-					done();
+		describe("Registered within the week" , function(){
+			var res;
+			before(function(done){
+				testSetup(function(error){
+					if(error)throw(error);
+					var currentDate  = moment().subtract(2,'day').format('YYYY-MM-DD').toString();
+					var query = "UPDATE "+db.tables.users.name+" SET created_at = '"+currentDate+"' WHERE number=9162728446 ";
+					db.get().query(query , function(err){
+						if(err)throw(err);
+						UserAnalytics.previousWeekRegistered(function(error , result){
+							res=result;
+							done();
+						});
+					
+					})
 				});
 			});
+			it("correct user count is returned" , function(){
+				assert.ok(res.totalUserCount === 1);
+			});
+			it("success is true" , function(){
+				assert.ok(res.success);
+			});
+	
 		});
-		it("correct user count is returned" , function(){
-            assert.ok(res.totalUserCount === 1);
-		});
-		it("success is true" , function(){
-			assert.ok(res.success);
-		});
-
-    });
-    describe("Get Total User registered previous month" , function(){
-		var res;
-        before(function(done){
-			testSetup(function(error){
-				if(error)throw(error);
-				UserAnalytics.previousMonthRegistered(function(error , result){
-					res=result;
-					done();
+		describe("Registered before the week time" , function(){
+			var res;
+			before(function(done){
+				testSetup(function(error){
+					if(error)throw(error);
+					var currentDate  = moment().subtract(10,'day').format('YYYY-MM-DD').toString();
+					var query = "UPDATE "+db.tables.users.name+" SET created_at = '"+currentDate+"' WHERE number=9162728446 ";
+					db.get().query(query , function(err){
+						if(err)throw(err);
+						UserAnalytics.previousWeekRegistered(function(error , result){
+							res=result;
+							done();
+						});
+					
+					})
 				});
 			});
+			it("correct user count is returned" , function(){
+				assert.ok(res.totalUserCount === 0);
+			});
+			it("success is true" , function(){
+				assert.ok(res.success);
+			});
+	
 		});
-		it("correct user count is returned" , function(){
-			assert.ok(res.totalUserCount === 1);
+		
+	});
+	describe("Get Total User Registered previous month" , function(){
+		describe("Registered within the month" , function(){
+			var res;
+			before(function(done){
+				testSetup(function(error){
+					if(error)throw(error);
+					var currentDate  = moment().subtract(6,'day').format('YYYY-MM-DD').toString();
+					var query = "UPDATE "+db.tables.users.name+" SET created_at = '"+currentDate+"' WHERE number=9162728446 ";
+					db.get().query(query , function(err){
+						if(err)throw(err);
+						UserAnalytics.previousWeekRegistered(function(error , result){
+							res=result;
+							done();
+						});
+					
+					})
+				});
+			});
+			it("correct user count is returned" , function(){
+				assert.ok(res.totalUserCount === 1);
+			});
+			it("success is true" , function(){
+				assert.ok(res.success);
+			});
+	
 		});
-		it("success is true" , function(){
-			assert.ok(res.success);
+		describe("Registered before the week time" , function(){
+			var res;
+			before(function(done){
+				testSetup(function(error){
+					if(error)throw(error);
+					var currentDate  = moment().subtract(40,'day').format('YYYY-MM-DD').toString();
+					var query = "UPDATE "+db.tables.users.name+" SET created_at = '"+currentDate+"' WHERE number=9162728446 ";
+					db.get().query(query , function(err){
+						if(err)throw(err);
+						UserAnalytics.previousWeekRegistered(function(error , result){
+							res=result;
+							done();
+						});
+					
+					})
+				});
+			});
+			it("correct user count is returned" , function(){
+				assert.ok(res.totalUserCount === 0);
+			});
+			it("success is true" , function(){
+				assert.ok(res.success);
+			});
+	
 		});
-
+		
 	});
 	afterEach(function(done){
         db.drop([db.tables.users.name], function(){
