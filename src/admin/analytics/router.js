c42fa7310d3177e9a497805fc37aa89c6d962a4d
Devesh Anand <ananddevesh29.devp@outlook.com>
2018-07-08 19:43:27 +0530

    minor

--- src/admin/analytics/router.js
+++ src/admin/analytics/router.js
@@ -4,8 +4,8 @@ var controller = require('./controller');
 var VerifyToken = require('../VerifyToken');
 
 router.get('/membership/totalUser',VerifyToken.allowCore,controller.getTotalUser);
-router.get('/membership/previousWeek',VerifyToken.allowCore,controller.previousWeekRegistered);
-router.get('/membership/previousMonth',VerifyToken.allowCore,controller.previousMonthRegistered);
+router.get('/membership/previousWeek',VerifyToken.allowCore,controller.previousWeeks);
+router.get('/membership/previousMonth',VerifyToken.allowCore,controller.previousMonths);
 router.post('/membership/custom',VerifyToken.allowCore,controller.custom);
 
 module.exports = router;
---
+++ src/app/models/food/MenuCustomisation.js
@@ -0,0 +1,7 @@
+var db = require('../../lib/sadda-db');
+var error_messages = require('../../config/error_messages');
+var QueryBuilder = require('../../lib/db-utils').QueryBuilder;
+var Log = require('../../lib/log');
+var Menu = require('./Menu');
+var validator = require('../../utility/validator');
+
