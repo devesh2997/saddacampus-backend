08540c56ba659d6e26f2ffdb9731e2ad743e94b3
AKASHAGARWAL0403 <32567062+AKASHAGARWAL0403@users.noreply.github.com>
2018-07-09 14:02:38 +0530

    Akash (#3)
    * count user function added
    
    * adding the test file
    
    * see comments
    
    * minor changes
    
    * adding drop and create user
    
    * minor changes
    
    * minor changes1'
    
    * indentation
    
    * minor changes
    
    * useranalytics spec improvements
    
    * minor
    
    * minor
    
    * minor
    
    * test cases
    
    * minor
    
    * modified test
    
    * adding month and week and custom func
    
    * changes in custom
    
    * minor changes
    
    * ADDING getAll function
    
    * minor
    
    * minor
    
    * minor

--- src/admin/analytics/router.js
+++ src/admin/analytics/router.js
@@ -4,8 +4,10 @@ var controller = require('./controller');
 var VerifyToken = require('../VerifyToken');
 
 router.get('/membership/totalUser',VerifyToken.allowCore,controller.getTotalUser);
+router.get('/membership/previousDay',VerifyToken.allowCore,controller.previousDays);
 router.get('/membership/previousWeek',VerifyToken.allowCore,controller.previousWeeks);
 router.get('/membership/previousMonth',VerifyToken.allowCore,controller.previousMonths);
+router.get('/membership',VerifyToken.allowCore,controller.getAll)
 router.post('/membership/custom',VerifyToken.allowCore,controller.custom);
 
 module.exports = router;
